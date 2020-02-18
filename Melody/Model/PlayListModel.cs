888889fcc65a757481ed2c22b09ffe1c9ff6e65d
nskumar73 <59399959+nskumar73@users.noreply.github.com>
2020-02-17 23:51:18 -0800

    Added new model for PlayListModel (#20)
    * Added new model for Paylist
    
    * corrected the spelling

---
+++ Melody/Model/PlayListModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melody.Model
+{
+    /// <summary>
+    /// This is model class for Play list
+    /// </summary>
+    public sealed class PlayListModel
+    {
+        /// <summary>
+        /// Name of the artist
+        /// </summary>
+       public string Name { get; set; }
+
+        /// <summary>
+        /// Genre of the music
+        /// </summary>
+        public string Genre { get; set; }
+        
+
+        /// <summary>
+        /// Artist Name
+        /// </summary>
+        public string Artist { get; set; }
+    }
+}
