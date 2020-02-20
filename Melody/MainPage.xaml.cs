03db75b5ec5917953057cf132c60df6372714dd7
Brillan <46096098+bpm150@users.noreply.github.com>
2020-02-19 22:55:34 -0800

    Added blue background to Melody banner art image placeholder (to make it easier to see the outline of it Created PlayListManager view model class (though we may not need it) Refactored original concept for three nested grids to instead be two nested grids with the bottom row of the inner grid spanning both columns of that grid Fixed image reference to the Melody banner art to reflect relative path in Assets folder Started the Playlist ListView, and the List of PlayListModel, for which the PlayListModel type will likely change

--- Melody/MainPage.xaml.cs
+++ Melody/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Melody.Model;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,11 +23,16 @@ namespace Melody
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        
-        //Test comment for XAML pull
+        private List<PlayListModel> playLists;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            playLists = new List<PlayListModel>();
+
+
+            // Add playlists to list of playlists
 
         }
     }
