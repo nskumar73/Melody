78edabb24398bf611c0db79eaeb75dd3d8a9e194
Brillan <46096098+bpm150@users.noreply.github.com>
2020-02-21 10:57:21 -0800

    Put the playlist cover image and the name of the playlist in a StackPanel Add 10 dummy PlayList objects to the ObservableCollection<PlayList>

--- Melody/MainPage.xaml.cs
+++ Melody/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Melody.Model;
 using System.Collections.ObjectModel;
+using System;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -24,6 +25,10 @@ namespace Melody
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        // PLAYLIST_COUNT guaranteed to be static
+        // because it is const, correct?
+        const int PLAYLIST_COUNT = 10;
+        
         private readonly ObservableCollection<PlayList> playLists;
 
         public MainPage()
@@ -31,10 +36,24 @@ namespace Melody
             this.InitializeComponent();
 
             playLists = new ObservableCollection<PlayList>();
+            for (var num = 1; num <= PLAYLIST_COUNT; ++num)
+            {
+                playLists.Add(new PlayList($"Placeholder Playlist {num}"));           
+            }
 
-            playLists.Add(new PlayList());
+            // Make some dummy playlists for the ObservableCollection
+            //var playListCount = new int[PLAYLIST_COUNT];
+            //foreach (var num in playListCount)
+            //{
+            // why is num == 0 on each loop iteration?
+            //    playLists.Add(new PlayList($"Placeholder Playlist {num.ToString()}"));
+            //}
 
-            // Add playlists to list of playlists
+            // Using Range was my first preference before using foreach
+            // Why doesn't the new Range feature from C# 8 work? >_<
+            // I was so excited, too
+            // https://docs.microsoft.com/en-us/dotnet/api/system.range?view=netcore-3.1
+
 
         }
     }
