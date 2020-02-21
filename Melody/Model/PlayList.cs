453d33e9e933cdad0208af4c97b61c4151605232
Brillan <46096098+bpm150@users.noreply.github.com>
2020-02-21 10:55:20 -0800

    PlayList class: Cover property is now CoverFilePath to better reflect what the property actually is All PlayList use the same placeholder cover Contains List of Song (formerly PlayListModel) Noted future need to implement INotifyPropertyChanged in order for changes to PlayList objects to be reflected in the ui

--- Melody/Model/PlayList.cs
+++ Melody/Model/PlayList.cs
@@ -7,12 +7,24 @@ using System.Threading.Tasks;
 namespace Melody.Model
 {
     //Playlist model to define playlist collection (e.g. All Songs)
-    public class PlayList
+    public sealed class PlayList // : INotifyPropertyChanged (see TODO)
     {
         //Properties of playlist
         public string Name { get; set; }
-        public string Cover { get; set; }
-        //Playlistmodel type should be renamed to song
-        public List<PlayListModel> Songs { get; set; }
+        public string CoverFilePath { get; set; }
+        //Playlistmodel type should be renamed to Song
+        private readonly List<Song> songs;
+
+        public PlayList(string name)
+        {
+            Name = name;
+            CoverFilePath = "/Assets/PlaylistCoverPlaceholder.png";
+            songs = new List<Song>();
+        }
+
+        // TODO: PlayList (or maybe a wrapper around it?)
+        // must implement INotifyPropertyChanged in order for the UI to update
+        // when PlayList objects inside it change state
+        // https://docs.microsoft.com/en-us/dotnet/api/system.componentmodel.inotifypropertychanged?view=netframework-4.8
     }
 }
