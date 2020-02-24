90e77c35cefb9a4131beb7d70926b36eb1699f34
Brillan <46096098+bpm150@users.noreply.github.com>
2020-02-24 12:54:16 -0800

    Fixed the data binding of the PlayListSongSelectionEditView ListView to be an ObservableCollection<Song> filled out by the ViewModel (instead of the placeholder direct, non-updating binding to the all songs playlist)
    PlayListCreationView and PlayListPlayBackView take up the same page real estate. PlayListCreationView is Visible by default, PlayListPlayBackView is Collapsed/hidden by default, these visible states are managed in MainPage event handlers
    
    Added PlayListPlayBackView ListView...this is the ui control that appears on the right side of the page and shows the user the songs in the selected playlist. Collapsed/hidden by default
    
    Dropped in the MediaPlayerElement for layout purposes. Doesn't work with any of the sample media files that I've tried with it

--- Melody/MainPage.xaml.cs
+++ Melody/MainPage.xaml.cs
@@ -15,8 +15,8 @@ using Windows.UI.Xaml.Navigation;
 using Melody.Model;
 using System.Collections.ObjectModel;
 using System;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Melody.ViewModel;
+using Windows.Media.Core;
 
 namespace Melody
 {
@@ -24,71 +24,32 @@ namespace Melody
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
-    {
-        // DUMMY_PLAYLIST_COUNT guaranteed to be static
-        // because it is const, correct?
-        const int DUMMY_PLAYLIST_COUNT = 10;
-        const int DUMMY_SONGS_COUNT = 8;
+    {   
+        private readonly ObservableCollection<PlayList> displayingPlayLists;
+        private readonly ObservableCollection<Song> displayingSongs;
 
-        
-        private readonly ObservableCollection<PlayList> playLists;
-
-        // Would be readonly if the reference was set in the MainPage constructor
-        // This is a placeholder for building the UI anyway
-        private PlayList allSongs;
+        // Which playlist is represented by these songs is not explicitly stored
+        // Rather, handled entirely in the PlayListMenuSidebarView_ItemClick event handler
 
         public MainPage()
         {
+            displayingPlayLists = new ObservableCollection<PlayList>();
+
+            //!! Recall that the Songs property on a PlayList is a List<Song>, 
+            // not an ObservableCollection<Song>
+            displayingSongs = new ObservableCollection<Song>();
+                        
+            PlayListManager.Setup();
+
+            PlayListManager.GetAllPlayLists(displayingPlayLists);
+            PlayListManager.GetAllSongs(displayingSongs);
+
             this.InitializeComponent();
 
-            playLists = new ObservableCollection<PlayList>();
-            ConstructPlaceholderDataObjects();
 
-        }
-
-        public void ConstructPlaceholderDataObjects()
-        {
-            // Create the dummy All Songs playlist
-            allSongs = new PlayList("All Songs");
-            // Populate with dummy songs
-            // (Represents the entire library of songs)
-            for (var num = 1; num <= DUMMY_SONGS_COUNT; ++num)
-            {
-                allSongs.Songs.Add(new Song
-                {
-                    Name = $"Placeholder Song Name {num}",
-                    Artist = "Placeholder Song Artist",
-                    Genre = "Placeholder Song Genre"
-                });
-
-            }
-
-            // First PlayList in the collection will be All Songs
-            // Or not? Maybe need to special-case this?
-            playLists.Add(allSongs);
-
-
-            for (var num = 1; num <= DUMMY_PLAYLIST_COUNT; ++num)
-            {
-                playLists.Add(new PlayList($"Placeholder Playlist Name {num}"));
-            }
-
-            // Make some dummy playlists for the ObservableCollection
-            //var playListCount = new int[DUMMY_PLAYLIST_COUNT];
-            //foreach (var num in playListCount)
-            //{
-            // why is num == 0 on each loop iteration?
-            //    playLists.Add(new PlayList($"Placeholder Playlist {num.ToString()}"));
-            //}
-
-            // Using Range was my first preference before using foreach
-            // Why doesn't the new Range feature from C# 8 work? >_<
-            // I was so excited, too
-            // https://docs.microsoft.com/en-us/dotnet/api/system.range?view=netcore-3.1
-
-
-
-
+            //MediaPlayerElement mediaPlayerElement1 = new MediaPlayerElement();
+            //mediaPlayerElement1.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Media/video1.mp4"));
+            //mediaPlayerElement1.AutoPlay = true;
         }
 
         private void PlayListSongSelectionEditView_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -115,6 +76,8 @@ namespace Melody
             // Provided by the control to be the content of the Text field
             // when it is queried by other controls
             var newPlaylist = new PlayList(PlayListName_UserInput.Text);
+            // I think the answer is to set the Text property directly in Xaml
+            // instead of using the PlaceholderText property
 
             foreach (var song in PlayListSongSelectionEditView.SelectedItems)
             {
@@ -125,7 +88,109 @@ namespace Melody
                 newPlaylist.Songs.Add((Song)song);
             }
 
-            playLists.Add(newPlaylist);
+            // TODO: Have the ViewModel make this playlsit
+            // give them the list of songs and the name that the user specified
+
+
+
+            // DONE: Fix the fact that here we are adding the new playlist directly to the 
+            // ObservableCollection<PlayList>
+            // Only the ViewModel should write to this collection
+            // In the Xamarin example, how does the View pass data to the
+            // ViewModel?
+            // Seems like the View (that is, right here in this event handler)
+            // should call a method on the ViewModel class and passing it the
+            // new playlist (then not keeping a reference to that new playlist
+            // here in the View
+            // ViewModel should be responsible for keeping track of the Playlist
+            // from that point forward and (as relevant) populating it in the
+            // ObservableCollection<PlayList> collection
+            PlayListManager.AddNewPlayList(newPlaylist);
+
+            // Update the View
+            PlayListManager.GetAllPlayLists(displayingPlayLists);
+
+
+            // TODO: Find out what the observer pattern says about
+            // the View's ObservableCollection being updated directly by the ViewModel
+
+
+            // ** QUESTION/IDEA: ?? Let the ViewModel keep a handle to the
+            // View's ObservableCollection so that it can update it
+            // when relevant...use case is for if playlists change
+            // for some reason other than because of a UI event
+            // A ViewModel's job is to be the bridge between the View and
+            // the Model, that is each ViewModel is specialized to connect
+            // specific View(s) with specific Model(s)
+        }
+
+
+        // When a playlist item is clicked
+        private void PlayListMenuSidebarView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var clickedPlayList = (PlayList)e.ClickedItem;
+            // Populate the ObservableCollection<Song> displayingSongs with
+            // the songs in the specified PlayList
+            //PlayListManager.GetSongsByPlayList(displayingSongs, clickedPlayList.Name);
+
+
+            PlayListManager.GetSongsByPlayList(displayingSongs, clickedPlayList);
+            
+            // View needs a list of the stuff that the view is supposed to display
+            // The view is not supposed to generate that list
+            // Even if it's trivial
+
+
+            //10k songs in it
+            // ui that is paginated
+            // give me page 8 ofthis list
+            // not doign that in the ui thread
+
+
+            // another reason
+            // dynamic playlist possible
+            // all songs might be a dynamic playlist
+
+            // songmanager has handle to all songs
+            // new class that uses the 
+
+            // it may not have a list of songs
+            // when you ask me for a list of all the songs
+            // I ask the songmanager for all the songs
+            // go to sound manager
+
+            // dynamic playlist random sort
+
+
+
+            //displayingSongs.Clear();
+
+            //foreach (var song in clickedPlayList.Songs)
+            //{
+            //    displayingSongs.Add(song);
+            //}
+
+            // Hide the playlist creator view
+            PlayListCreationView.Visibility = Visibility.Collapsed;
+            // Show the playlist playback view set to the clicked playlist
+            PlayListPlayBackView.Visibility = Visibility.Visible;
+
+            // If we ask the/a ViewModel to fill out the songs, we need to tell them
+            // which songs somehow...that is, which playlist
+            // Options include:
+            // PlayList by its string Name (presuming that we don't allow duplicate names)
+            // Reference to the PlayList directly...
+
+            // MVVM probably wants us to communicate about the PlayList by Name
+            // *sigh* is there a better way to do this and still be honoring
+            // MVVM?
+
+            // These aren't simply equivelant PlayList objects, they are
+            // guaranteed to be exactly the same PlayList object, correct?
+            // Makes best sense to...
+
+            // Who's job should it be (View or ViewModel) to populate
+            // ObservableCollection<Song> displayingSongs?
         }
     }
 }
